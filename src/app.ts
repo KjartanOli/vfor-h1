76683c1b4fc20aebb69bb38a9d9031a75c6490e3
Kjartan Oli Agustsson <kjartanoli@disroot.org>
2024-03-11 19:45:41 +0000

    Basic setup

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { router } from './routes/api.js';
+
+const app = express();
+
+app.get('/');
+app.use(express.json())
+app.use(router);
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
